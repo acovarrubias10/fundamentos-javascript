3c10a31db5cfb69527fa10664f180974c4f1361f
PaAdRaAg <pramos12@ucol.mx>
2019-09-17 19:40:14 -0500

    subiendo

---
+++ main.js
@@ -0,0 +1,41 @@
+/*
+    Universidad de Colima
+    Facultad de telemática
+    Fundamentos de programación 
+*/
+
+var nombre = 'Vanessa'
+var edad = 18
+var esEstudiante = true
+var email 
+var numero1 = 5
+var numero2 = 2
+
+console.log(50 + 10 / 8 + 2)
+console.group((50 + 10) / (8 + 2))
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 % numero2)
+
+var resultado = numero1 + numero2
+console.log(resultado)
+
+
+console.log (
+    nombre + ' tiene ' + edad + 'años. ¿Es estudiante? ' + esEstudiante
+)
+
+console.log(nombre)
+console.log(edad)
+console.log(email)
+
+edad = 'veintidos'
+console.log(edad)
+
+nombre = prompt('¿Cómo te llamas?')
+
+alert('Hola' + nombre)
+
